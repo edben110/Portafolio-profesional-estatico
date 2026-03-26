e3d99e15e85145d0b604f0ded3a4e149e7bced64
JUanM776 <Juan.cordobaflore@campusucc.edu.co>
2026-03-26 11:50:39 -0500

    feat: add hv app for Juan Manuel Cordoba Florez (seccion 4)

--- config/urls.py
+++ config/urls.py
@@ -15,11 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('seccion/4/', include('hv.urls')),
     path('seccion/<int:numero>/', views.seccion_placeholder, name='seccion_placeholder'),
     path('admin/', admin.site.urls),
 ]
--- config/views.py
+++ config/views.py
@@ -8,7 +8,7 @@ def home(request):
         {"numero": 1, "titulo": "Seccion 1"},
         {"numero": 2, "titulo": "Seccion 2"},
         {"numero": 3, "titulo": "Seccion 3"},
-        {"numero": 4, "titulo": "Seccion 4"},
+        {"numero": 4, "titulo": "Mi HV"},
     ]
     return render(request, "home.html", {"secciones": secciones})
 
